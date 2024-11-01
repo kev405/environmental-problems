bcfca6ca90d52af839956343eb7989cd1bab22e1
MargaritaGrisales <128561072+MargaritaGrisales@users.noreply.github.com>
2024-11-01 16:00:09 -0500

    Se agrega página Sobre Nosotros

---
+++ src/pages/nosotros/nosotros.jsx
@@ -0,0 +1,14 @@
+// src/pages/nosotros/nosotros.jsx
+import React from 'react';
+import './Nosotros.css'; 
+
+const Nosotros = () => {
+    return (
+        <div className="nosotros-container">
+            <h1>Sobre Nosotros</h1>
+            <p>Aquí puedes poner información sobre tu equipo, misión, etc.</p>
+        </div>
+    );
+};
+
+export default Nosotros;
